bfc7b7829e09c47452de78c72336ef066d0e0454
dainiskaulins <dainis.kaulins@gmail.com>
2026-07-02 13:09:17 +0300

    Update cards.js

--- common/cards.js
+++ common/cards.js
@@ -1,6 +1,33 @@
-// common/cards.js
+/*    common/cards.js
+=========================================================
+Kāršu attēlošana.
 
-function generateDeck() {
-    return [];
+Uzdevums:
+- pārvērš kāršu masīvu skaistā tekstā pa mastīm.
+
+2026-07-01
+=========================================================
+*/
+
+function formatHand(hand) {
+  const suits = [
+    { code: "S", symbol: "♠" },
+    { code: "H", symbol: "♥" },
+    { code: "D", symbol: "♦" },
+    { code: "C", symbol: "♣" }
+  ];
+
+  let lines = [];
+
+  for (const suit of suits) {
+    const cards = hand
+      .filter(card => card[0] === suit.code)
+      .map(card => card.slice(1))
+      .join(" ");
+
+    lines.push(suit.symbol + " " + cards);
+  }
+  return lines.join("<br>");
 }
-console.log("cards.js ielādēts");
+
+console.log("common/cards.js ielādēts");
